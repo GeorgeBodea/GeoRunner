fa3c6b2e4f9d3a4034a4d9e05e22c2fbfd9c56bf
ZamfirescuStefan <zamfirescu.stefan12@gmail.com>
2021-11-20 15:47:41 +0200

    Added change ball functionality

---
+++ Assets/Scripts/GameManagerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManagerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // void Start()
+    // {   
+    //     PlayerPrefs.SetString("PlayerMaterial", "Gold");
+    //     string ballMaterialName = PlayerPrefs.GetString("PlayerMaterial"); 
+    //     Material ballMaterial = Resources.Load<Material>("Materials/" + ballMaterialName);
+    //     if (ballMaterial)
+    //     {
+    //         print(ballMaterial);
+    //     }
+    //     else
+    //     {
+    //         print("Error to load this material: " + ballMaterialName);
+    //     }
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
