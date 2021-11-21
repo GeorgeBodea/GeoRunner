7ef65d1f371c002a187de37a3564ca4bf508a153
Denisa Iordache <61270889+denisaiordache@users.noreply.github.com>
2021-11-21 15:59:10 +0200

    Added Time Scoring

--- Assets/Scripts/SphereMovement.cs
+++ Assets/Scripts/SphereMovement.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
+
 
 public class SphereMovement : MonoBehaviour
 {
@@ -13,14 +16,23 @@ public class SphereMovement : MonoBehaviour
     private int remainingJumps = 2;
     private bool isGrounded;
 
+    public Text textScore;
+    private int score;
+    private string yourScore;
+    private float timer;
+    private bool isMoving;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
-        
+
+
         // Load the material 
         string playerMaterialName = PlayerPrefs.GetString(Constants.BallMaterial);
         GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/" + playerMaterialName);
+
+        
     }
 
     // Update is called once per frame
@@ -29,10 +41,38 @@ public class SphereMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             jumpKeyPressed = true;
+            
         }
 
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+
+        
+        //check if the ball started moving
+        if((horizontalInput > 0.0f) || (verticalInput > 0.0f))
+        {
+            isMoving = true;
+        }
+
+        if(isMoving)
+        {
+            //increasing score per second
+            timer += Time.deltaTime;
+
+            if (timer > 1f)
+            {
+
+                score += 1;
+
+                yourScore = "Score: " + score.ToString();
+                textScore.text = yourScore;
+
+                //Reset the timer to 0.
+                timer = 0;
+            }
+        }
+
+       
     }
 
     void FixedUpdate()
