f03dfc16b430aea07eff1ea691584953e1e2e050
Daniel7398 <62849456+Daniel7398@users.noreply.github.com>
2022-01-12 11:40:28 +0200

    GameOverMenu
    Added game over menu. Added play again button, main menu button and quit button.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,8 +7,8 @@ public class GameManager : MonoBehaviour
 {
     bool gameHasEnded = false;
 
-    public float restartDelay = 2f;
-    public float overDelay = 1f;
+    public float restartDelay = 1f;
+
 
     public GameObject gameIsOverUI;
 
@@ -15,21 +17,18 @@ public class GameManager : MonoBehaviour
         if (gameHasEnded == false)
         {
             gameHasEnded = true;
-            Invoke("GameOver1", overDelay);
+            Invoke("GameOver",restartDelay);
         }
 
     }
 
    
 
-    void GameOver1()
-    {
-        gameIsOverUI.SetActive(true);
-        Invoke("GameOver", restartDelay);
-    }
-
     void GameOver()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameIsOverUI.SetActive(true);
+        Time.timeScale = 0f;
     }
+
+
 }
---
+++ Assets/Scripts/GameOverMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
